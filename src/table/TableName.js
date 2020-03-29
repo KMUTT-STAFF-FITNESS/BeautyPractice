ca2c36616add881ad9f25c4fc8b5a06c385ec154
sarinratbeauty <sarinrat.beauty@mail.kmutt.ac.th>
2020-03-30 02:57:37 +0700

    🎵 [update] from state to hook

--- src/table/TableName.js
+++ src/table/TableName.js
@@ -1,50 +1,121 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Table from 'react-bootstrap/Table'
-import { Container,Nav } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import Sidebar from './core/Sidebar'
+import Dropdown from './core/Dropdown'
+
+export default class TableName extends Component (prop) {
+
+  const [name, setName] = useState([
+    {
+      fname: 'srr',
+      lname: 'bt'
+    },
+    {
+      fname: 'fame',
+      lname: 'bt'
+    },
+    {
+      fname: 'fameeeeeee',
+      lname: 'bt'
+    },
+  ])
+  const [unit, setUnit] = useState()
+  const [status, setStatus] = useState()
+  const [edit, setEdit] = useState()
+
+  render(){
+
+  return (
+    <>
+      <Table>
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Unit</th>
+            <th>Status</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {name.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.fname}</td>
+                <td>{data.lname}</td>
+                <td>{unit}</td>
+                <td>
+                  <Dropdown />
+                </td>
+                <td><a href={() => setEdit("/beauty")}>{edit}</a></td>
+              </tr>
+            )
+          }
 
 
-export default class TableName extends Component {
-    state = {
-        name: '',
-        surname: '',
-        unit: '',
-        status: ''
-    }
+          )}
+        </tbody>
+      </Table>
 
-    renderTableData = () => {
-        return (
-            <>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Surname</th>
-                        <th>Unit</th>
-                        <th>Status</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{this.state.name}</td>
-                        <td>{this.state.surname}</td>
-                        <td>{this.state.unit}</td>
-                        <td>{this.state.status}</td>
-                        <td><a href = "TableEdit.js">edit</a></td>
-                    </tr>
-                </tbody>
-            </>
-        )
-    }
-    render() {
-        return (
-            <div>
-                
-                <Container>
-                    <Table striped bordered hover>
-                        {this.renderTableData()}
-                    </Table>
-                </Container>
-            </div>
-        )
-    }
+    </>
+  )
+
 }
+
+
+
+}
+
+
+// export default class TableName extends Component {
+//     state = {
+//         name: 'aaa' ,
+//         surname: 'aaaa',
+//         unit: 'aaaaa',
+//         status: 'year'
+//     }
+
+//     renderTableData = () => {
+//         return (
+//             <>
+
+//                 <thead>
+//                     <tr>
+//                         <th>Name</th>
+//                         <th>Surname</th>
+//                         <th>Unit</th>
+//                         <th>Status</th>
+//                         <th>Edit</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>{this.state.name}</td>
+//                         <td>{this.state.surname}</td>
+//                         <td>{this.state.unit}</td>
+//                         <td>
+//                             {/* <Dropdown/> */}
+//                         </td>
+//                         <td><a href="/beauty">view</a></td>
+//                     </tr>
+//                 </tbody>
+//             </>
+//         )
+//     }
+//     render() {
+//         return (
+            // <div className="my-5">
+
+            //     <Sidebar />
+            //     <Container>
+            //         <Table striped bordered hover>
+            //             {this.renderTableData()}
+            //         </Table>
+            //     </Container>
+
+            // </div>
+//         )
+//     }
+// }
